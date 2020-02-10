0b05cecc85b3aef0758bd47709f9a4425a17aa4d
Jakub Sebok <Jakub.Sebok@external.erstegroup.com>
2020-02-10 16:12:08 +0100

    feat: 1. case implementation

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -3,8 +3,16 @@ export default class StringCalculator {
    * Simulates addition on numbers given on input.
    * @param input 0, 1 or 2 numbers connected via string
    * @return result of addition
+   * @throws Error on incorrect input
    */
   public add(input: String): number {
-    return 0;
+    if (!input) {
+      return 0;
+    }
+    const numbers: number[] = input.split(',').map(s => parseInt(s.trim()));
+    if (numbers.length > 3) {
+      throw new Error('Number of input elements cannot exceed 3');
+    }
+    return numbers.reduce((prev, next) => prev + next);
   }
 }
--- test/StringCalculator.spec.ts
+++ test/StringCalculator.spec.ts
@@ -6,13 +6,49 @@ describe('StringCalculator', () => {
 
   describe('add', () => {
 
-    it('should return 0', () => {
-      const input = 'something';
+    it('return 0 on empty input', () => {
+      const input = '';
 
       const result = calculator.add(input);
 
       expect(result).toEqual(0);
-    })
+    });
+
+    it('return same number when only 1 is given', () => {
+      const input = '1';
+
+      const result = calculator.add(input);
+
+      expect(result).toEqual(1);
+    });
+
+    it('return addition result when 2 numbers are given', () => {
+      const input = '1,2';
+
+      const result = calculator.add(input);
+
+      expect(result).toEqual(3);
+    });
+
+    it('return addition result when 3 numbers are given', () => {
+      const input = '1,2,3';
+
+      const result = calculator.add(input);
+
+      expect(result).toEqual(6);
+    });
+
+    it('fails when more than 3 numbers are given', () => {
+      const input = '1,2,3,4';
+
+      let result: number = 0;
+      try {
+        result = calculator.add(input);
+      } catch (e) {
+        expect(e.message).toEqual('Number of input elements cannot exceed 3');
+        expect(result).toBe(0);
+      }
+    });
 
   });
 
