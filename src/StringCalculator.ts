1ef13df5ef3710a71dcf811544f1ba2cba96676d
Jakub Sebok <Jakub.Sebok@external.erstegroup.com>
2020-02-10 19:21:51 +0100

    chore: refactor code for brevity and readability

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -2,6 +2,10 @@ export default class StringCalculator {
 
   private callsCount = 0;
 
+  /**
+   * Returns total count of {@link add}
+   * method calls on this instance.
+   */
   public getCalledCount(): number {
     return this.callsCount;
   }
@@ -10,24 +14,27 @@ export default class StringCalculator {
    * Simulates addition on numbers given on input.
    * @param input up to 3 numbers separated by comma or new line or delimiter on first line and numbers joined with this delimiter on second line
    * @return result of addition or 0 on wrong input
+   * @throws error when input has wrong format or negative numbers are supplied
    */
   public add(input: string): number {
     this.callsCount += 1;
+    
     if (!input) {
       return 0;
     }
-    const separatorCandidate = input.substr(0, input.indexOf('\n'));
     
     let numbers: number[];
+
+    const separatorCandidate = input.substr(0, input.indexOf('\n'));
     if (!separatorCandidate || !isNaN(parseInt(separatorCandidate))) {
       numbers = this.addOneLiner(input);
     } else {
       numbers = this.addTwoLiner(input);
     }
+
     const negatives = numbers.filter(n => n < 0);
-    if (negatives && negatives.length > 0) {
-      throw new Error('Negatives not allowed: ' + negatives.join(','));
-    }
+    this.failIf(negatives && negatives.length > 0, 'Negatives not allowed: ' + negatives.join(','));
+    
     numbers = numbers.filter(n => n <= 1000);
 
     return numbers.reduce((prev, next) => prev + next);
@@ -40,16 +47,20 @@ export default class StringCalculator {
 
   private addTwoLiner(input: string): number[] {
     const lines = input.split('\n').map(l => l.trim());
-    if (lines.length !== 2) {
-      throw new Error('Wrong input format');
-    }
+    this.failIf(lines.length !== 2, 'Wrong input format');
+    
     const separator = lines[0];
-    if (!separator) {
-      throw new Error('No separator specified');
-    }
+    this.failIf(!separator, 'No separator specified');
+
     if(!lines[1]) {
       return [0];
     }
     return lines[1].split(separator).map(s => parseInt(s));
+  }
+
+  private failIf(condition: boolean, message: string): void {
+    if (condition) {
+      throw new Error(message);
+    }
   }
 }
